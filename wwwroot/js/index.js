3e207c90250be7cc5280d59f5854a7ee7dcc5c7b
Ella <ella.vankirk@gmail.com>
2021-06-04 16:55:20 -0500

    Completed Javascript and building mvc from scratch module

--- wwwroot/js/index.js
+++ wwwroot/js/index.js
@@ -1,4 +1,24 @@
-﻿console.log("Hello PluralSight");
+﻿$(document).ready(function () {
+    console.log("Hello PluralSight");
 
-var theForm = document.getElementById("theForm");
-theForm.hidden = true;
+    var theForm = $("#theForm");
+    theForm.hide();
+
+    var button = $("#buyButton");
+    button.on("click", function () {
+        console.log("Buying Item");
+    });
+
+    var productInfo = $(".product-props li");
+    productInfo.on("click", function () {
+        console.log("You clicked on " + $(this).text());
+    });
+
+    var $loginToggle = $("#loginToggle");
+    var $popupForm = $(".popup-form");
+
+    $loginToggle.on("click", function () {
+        $popupForm.fadeToggle(1000);
+    });
+});
+
